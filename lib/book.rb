d136fe8d6002667473a20d92601b299723cc0bc8
meaghan jennings <meaghanljennings@gmail.com>
2020-08-11 19:00:44 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,12 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count #remove attr_accessor for genre
   attr_reader :title
+  #add an attr_reader for genre
+  attr_reader :genre
 
+  GENRES = []
+  
+  
   def initialize(title)
     @title = title
   end
@@ -10,4 +15,9 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+#create writer for genre and add logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
